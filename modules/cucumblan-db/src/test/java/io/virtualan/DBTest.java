4a4ba0010c25e3827f80593ea1d418792e466993
Elan Thangamani <elans3.java@gmail.com>
2021-09-26 21:24:15 -0500

    Select Sql

--- modules/cucumblan-db/src/test/java/io/virtualan/DBTest.java
+++ modules/cucumblan-db/src/test/java/io/virtualan/DBTest.java
@@ -10,7 +10,9 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "classpath:features",
     glue = {"io.virtualan.cucumblan.core"},
+
     plugin = {"pretty",
+        "io.virtualan.cucumblan.props.hook.FeatureScope",
         "json:target/cucumber-report.json"})
 
 public class DBTest {
